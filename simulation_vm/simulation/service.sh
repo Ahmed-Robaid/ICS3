84815015f69b296388165364c49c12af74b85fff
ARAM_AHMED <robaid77@gmail.com>
2024-09-27 23:44:49 +0300

    Create service.sh

---
+++ simulation_vm/simulation/service.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the service file path
+SERVICE_FILE="/etc/systemd/system/simulation.service"
+
+# Create the systemd service file
+cat <<EOL > $SERVICE_FILE
+[Unit]
+Description=Start Simulation on Boot
+
+[Service]
+Type=forking
+ExecStart=/bin/bash /home/$(logname)/ICS/simulation_vm/simulation/remote_io/modbus/run_all.sh
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Reload the systemd daemon to recognize the new service
+systemctl daemon-reload
+
+# Enable the service to start on boot
+systemctl enable simulation.service
+
+# Start the service immediately
+systemctl start simulation.service
+
+# Check the status of the service
+systemctl status simulation.service
+
