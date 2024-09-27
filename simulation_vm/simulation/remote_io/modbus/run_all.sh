5d19ad881a3aa0f5e7a4ac7470b8df1458b2fedb
ARAM_AHMED <robaid77@gmail.com>
2024-09-27 22:29:00 +0300

    Update run_all.sh

--- simulation_vm/simulation/remote_io/modbus/run_all.sh
+++ simulation_vm/simulation/remote_io/modbus/run_all.sh
@@ -2,6 +2,8 @@
 
 sim_path=/home/ahmed/ICS/simulation_vm/simulation/remote_io/modbus
 sudo pkill python3
+sudo pkill simulation
+$sim_path/../../simulation &
 sudo python3 $sim_path/feed1.py &
 sudo python3 $sim_path/feed2.py &
 sudo python3 $sim_path/purge.py &
